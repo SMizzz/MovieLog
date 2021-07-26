c20069298855ec0bbc47408fc438d1bee81e82ab
SMizzz <shinn.mizzz@gmail.com>
2021-07-26 23:53:22 +0900

    DataManager and CoreData Setting

--- MovieLog/Compose/ComposeViewController.swift
+++ MovieLog/Compose/ComposeViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var textView: UITextView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  @IBAction func onDragSlider(_ sender: UISlider) {
   }
 }
