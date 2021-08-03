b05d8720e06c1f1fc84e745de4f36ab99cf8daeb
SMizzz <shinn.mizzz@gmail.com>
2021-08-03 16:31:48 +0900

    Movie API Refactoring

--- MovieLog/MovieAPI/MovieAPI.swift
+++ MovieLog/MovieAPI/MovieAPI.swift
@@ -18,7 +18,8 @@ enum MovieAPI {
 
 extension MovieAPI: TargetType {
   var baseURL: URL {
-    guard let url = URL(string: "https://api.themoviedb.org/3") else { fatalError("url error") }
+    guard let url = URL(string: "https://api.themoviedb.org/3")
+    else { fatalError("url error") }
     return url
   }
   
